ddc3e3bbc93a6d43d58a934da0afa7ad1a2bc005
chandrarizky <chandrarizkyraharja@gmail.com>
2020-11-18 08:17:15 +0700

    workspace add aot privacy policy(example)

--- app/src/main/java/com/abstraksi/btswallpaperhd2020/config/ContentConfig.kt
+++ app/src/main/java/com/abstraksi/btswallpaperhd2020/config/ContentConfig.kt
@@ -13,8 +13,8 @@ var JUMLAH_PHOTO_PER_ALBUM = 50
 var INIT_JUMLAH_PHOTO_PER_ALBUM = 50
 
 // fcm topic untuk push notitifcation
-//const val APP_FCM_TOPIC = "riefistapp_catwallpaper_topic"
+//const val APP_FCM_TOPIC = "riefistapp_catwallpaper_tospic"
 const val APP_FCM_TOPIC = "abstraksi_btswallpaper_topic"
 
 const val SHOW_PRIVACY_POLICY = false // ubah ke false jika tidak ingin menggunakan privacy policy
-const val PRIVACY_POLICY_URL = "https://www.google.com"
+const val PRIVACY_POLICY_URL = "https://privacy-policy-aot-wallpaper.netlify.app/"
